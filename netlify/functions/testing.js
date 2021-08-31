5059b3c36983c1b2dfb496031b2aa7c4b45f93a3
Marcos875433 <marcosclashofclans29@gmail.com>
2021-08-30 21:38:07 -0500

    no creo que funcione, pero habra que probar xd

--- netlify/functions/testing.js
+++ netlify/functions/testing.js
@@ -1,10 +1,69 @@
+const express = require('express')
+const crypto = require('crypto');
+const twitchSigningSecret = 'cabezadetornilloazul';
 exports.handler = async (event) => {
     const { headers = [] } = event;
     
     const type = headers['twitch-eventsub-message'] || 'no type';
     const eventType = headers['twitch-eventsub-subscription-type']
+    const verifyTwitchSignature = (req, res, buf, encoding) => {
+        const messageId = headers["Twitch-Eventsub-Message-Id"];
+        const timestamp = headers["Twitch-Eventsub-Message-Timestamp"];
+        const messageSignature = headers["Twitch-Eventsub-Message-Signature"];
+        const time = Math.floor(new Date().getTime() / 1000);
+        console.log(`Message ${messageId} Signature: `, messageSignature);
+      
+        if (Math.abs(time - timestamp) > 600) {
+          // needs to be < 10 minutes
+          console.log(`Verification Failed: timestamp > 10 minutes. Message Id: ${messageId}.`);
+          throw new Error("Ignore this request.");
+        }
+      
+        if (!twitchSigningSecret) {
+          console.log(`Twitch signing secret is empty.`);
+          throw new Error("Twitch signing secret is empty.");
+        }
+      
+        const computedSignature =
+          "sha256=" +
+          crypto
+            .createHmac("sha256", twitchSigningSecret)
+            .update(messageId + timestamp + buf)
+            .digest("hex");
+        console.log(`Message ${messageId} Computed Signature: `, computedSignature);
+      
+        if (messageSignature !== computedSignature) {
+          throw new Error("Invalid signature.");
+        } else {
+          console.log("Verification successful");
+        }
+    };
 
-    if (type !== 'notification' || eventType !== 'channel.subscription.message') {
+    app.use(express.json({ verify: verifyTwitchSignature }));
+
+    app.post('/', async (req, res) => {
+    const messageType = headers['twitch-eventsub-message-type'];
+    if (messageType === 'webhook_callback_verification') {
+        console.log('Verifying Webhook');
+        return res.status(200).send(req.body.challenge);
+    }
+
+    const { type } = req.body.subscription;
+    const { event } = req.body
+
+    console.log(
+        `Receiving ${type} request for ${event.broadcaster_user_name}: `,
+        event
+    );
+
+    res.status(200).end();
+    });
+
+    const listener = app.listen(port, () => {
+        console.log('Your app is listening on port ' + listener.address().port);
+    });
+
+    if (eventType !== 'channel.subscription.message') {
         return {statusCode:200, body: 'c'};
     }
 
@@ -15,6 +74,8 @@ exports.handler = async (event) => {
     
     const { event: twitchEvent } = JSON.parse(event.body);
     const user = twitchEvent.user_name;
+
+    console.log(user)
     
     return {
         statusCode: 200,
